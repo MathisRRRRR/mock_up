388362e381135764c28830bd9fe40b0b3f73a528
Joshua Ofori <oforijoshua37@gmail.com>
2020-10-09 17:12:20 +0200

    Quiz page two and answers

--- template/footer.php
+++ template/footer.php
@@ -1,12 +1,6 @@
 <footer>  
     <p>Author: Hege Refsnes</p>
     <p>
-        <display 
-        <a href="mathis.rousseau@isen.yncrea.fr">mathis.rousseau@isen.yncrea.fr</a>
-        <a href="romuald-koami-mawupe.aziagba@rabat.yncrea.fr">mathis.rousseau@isen.yncrea.fr</a>
-        <a href="joshua.ofori@student.yncrea.fr">joshua.ofori@student.yncrea.fr</a>
-        <a href="simon.goncalves@student.yncrea.fr">simon.goncalves@student.yncrea.fr</a>
-        <a href="theodore.westeel@isen.yncrea.fr">theodore.westeel@isen.yncrea.fr</a>
-        <a href="arnaud.francoise@student.yncrea.fr">arnaud.francoise@student.yncrea.fr</a>
+        <a href="mailto:hege@example.com">hege@example.com</a>
     </p>
 </footer>
