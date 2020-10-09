18abbe0636345cc6f23d2e2908187c6ac9e470b2
@ <farnaud@astrorun.com>
2020-10-09 14:28:55 +0200

    Auto stash before merge of "step1" and "origin/step1"

--- template/header.php
+++ template/header.php
@@ -1,7 +1,7 @@
 <nav>
   <ul>
     <li><a class="active" href="./mock_up/">Home</a></li>
-    <li><a href="./template/quizz.php">Quizz</a></li>
+    <li><a href="./quizz.php">Quizz</a></li>
     <!-- Button to open the modal login form -->
 
     <li><a onclick="document.getElementById('id01').style.display='block'" >Login</a></li>
