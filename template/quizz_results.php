b9554d203f0faf0a0e216c40bd10adee33a589e0
Joshua Ofori <oforijoshua37@gmail.com>
2020-10-23 14:28:24 +0200

    answerpage

--- template/quizz_results.php
+++ template/quizz_results.php
@@ -14,15 +14,17 @@
  <?php
 
 if(isset($_POST['submit'])){
-   $answers = 1;
-   $answer_id = $_POST[$answers];
+   //$answers = 1;
+   // var_dump($_POST[$answers]);
+   
+
 
   
    
    
-      $insert = $database->query(" INSERT INTO `user_answer`(`user_id`,`answer_id`) VALUES ($answers, $answer_id) ");
-      var_dump($insert);
-      
+      $insert = $database->exec(" INSERT INTO `user_answer`(`user_id`,`answer_id`) VALUES (1, 5) ");
+
+
       if($insert){
          echo("inserted");
       }
