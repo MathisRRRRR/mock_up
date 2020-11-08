c0192ac1e762edafc47e5cd32f9214fc267322eb
AZIAGBA <romuald-koami-mawupe.aziagba@student.yncrea.fr>
2020-11-08 20:19:20 +0100

    modif  index

--- index.php
+++ index.php
@@ -1,32 +1,21 @@
 <?php
 //TODO start session
-session_start();
 
 //TODO include database.php file
-include("database.php");
-
+?>
+<?php
 //TODO assign database connexion into $database variable
-$database = databaseConnection();
 
-//TODO include checkUser.php file
-include("checkUser.php");
+//TODO include checkUser.php file?>
 
+<?php
 //TODO (in the next step) control user access
 
 //TODO get page parameter ($_GET['page'] or $_POST['page']) and assign it into $page variable
-//if 'action/'.$page'.php' exists then include it (use file_exists($filename) function)
-$page='';
-$filename ='template/account_page.php';
-if(isset($_GET['page'])){
-    $page = $_GET['page'];
 
-    if(file_exists('template/'.$page.'.php')){
-        $filename = 'template/'.$page.'.php';
-    }
-}
+//if 'action/'.$page'.php' exists then include it (use file_exists($filename) function)?>
 
-
-
+<?php
 
 //create one php file for each action to manage on the website
 
@@ -37,42 +26,25 @@ if(isset($_GET['page'])){
 
 // TODO insert the start html envelope (<html><head>....</head><body>
 
-// TODO using $page decide to include header.php
+// TODO using $page decide to include header.php?>
 
-
+<?php
 
 //TODO add header display
-?>
-
-<!Doctype html >
-
-<?php
-include("main.php");
-?>
-
-<body>
-<?php
-if(!isset($_GET['page']) && $page !==''){
-    
-    include("template/header.php");     
-    
-
-    include('template/footer.php');
-}
-include($filename); 
-?>
-</body>
-
-</html>
-
-
-<?php
 
 //TODO if 'view/'.$page'.php' exists then include it (use file_exists($filename) function)
-//           else include 'view/main.php' (it has to exist)
+//           else include 'view/main.php' (it has to exist)?>
 
-//create one php file for each view to manage on the website (don't forget to create on main.php view)
+<?php
 
+//create one php file for each view to manage on the website (don't forget to create on main.php view)?>
+<?php include("main.php"); ?>
+<?php include("template/header.php"); ?>
+
+<p>Projet Web</p>
+<?php
+    include('template/footer.php'); ?>
+<?php
 //TODO use
 //             input params (included in $_GET or $_POST)
 //             $database variable (initialized in $database.php)
@@ -80,4 +52,4 @@ include($filename);
 
 // add view display possibly using data from database
 
-// TODO insert the end html envelope (</body></html>)
+// TODO insert the end html envelope (</body></html>)?>
